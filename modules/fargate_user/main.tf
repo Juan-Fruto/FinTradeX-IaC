25878895035ad4bb76c47fbcc3ef8654bf66b4d4
Juan-Fruto <jfruto@ucol.mx>
2025-11-05 13:42:52 -0600

    add dummy tomcat server

---
+++ modules/fargate_user/main.tf
@@ -0,0 +1,179 @@
+########################
+# Networking Resources #
+########################
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+######################
+# ECS + Fargate Setup #
+######################
+
+resource "aws_ecs_cluster" "tomcat_cluster" {
+  name = "tomcat-cluster"
+}
+
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "app_lb" {
+  name               = "tomcat-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+}
+
+resource "aws_lb_target_group" "tg" {
+  name         = "tomcat-tg"
+  port         = 8080
+  protocol     = "HTTP"
+  vpc_id       = aws_vpc.main.id
+  target_type  = "ip"  # ✅ required for Fargate
+
+  health_check {
+    path = "/"
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+#########################
+# ECS Task + Service    #
+#########################
+
+resource "aws_ecs_task_definition" "tomcat_task" {
+  family                   = "tomcat-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities  = ["FARGATE"]
+  cpu                      = "512"
+  memory                   = "1024"
+  execution_role_arn       = aws_iam_role.ecs_task_exec.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "tomcat"
+      image     = "tomcat:9.0"
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_iam_role" "ecs_task_exec" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_policy" {
+  role       = aws_iam_role.ecs_task_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_service" "tomcat_service" {
+  name            = "tomcat-service"
+  cluster         = aws_ecs_cluster.tomcat_cluster.id
+  task_definition = aws_ecs_task_definition.tomcat_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+    assign_public_ip = true
+    security_groups  = [aws_security_group.alb_sg.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.tg.arn
+    container_name   = "tomcat"
+    container_port   = 8080
+  }
+
+  depends_on = [aws_lb_listener.listener]
+}
+
+########################
+# Output
+########################
+output "load_balancer_dns" {
+  value = aws_lb.app_lb.dns_name
+}
--- provider.tf
+++ provider.tf
@@ -10,3 +10,10 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+module "fargate_user" {
+  source = "./modules/fargate_user"
+  providers = {
+    aws = aws
+  }
+}
